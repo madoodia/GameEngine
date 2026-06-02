4cebccb3fb4c85796beae07f6727f88cda7f553b
Reza Aarabi <madoodia@gmail.com>
2026-06-01 17:36:28 -0700

    New Development started

--- Engine/Source/Editor/dummy.cpp
+++
@@ -1,6 +0,0 @@
-#include "Engine.h"
-
-const char * GetEngineVersion()
-{
-	return "Engine Version 1.0.0";
-}
--- Playground/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "Runtime/Engine.h"
-
-#include <cstdio>
-
-int main()
-{
-	printf("Engine Version: %s\n", GetEngineVersion());
-	return 0;
-}
